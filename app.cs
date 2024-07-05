e45c858ab62e9e1e57ae3f2de798f17bb14a840c
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2024-07-05 21:55:54 +0200

    Create app.cs

---
+++ app.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace SimpleMathLibrary
+{
+    public static class SimpleMath
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Het tweede getal mag niet nul zijn.");
+            }
+            return a / b;
+        }
+    }
+}
